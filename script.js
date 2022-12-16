7435393d2d37ffb032b4712b9aa8983d596c83b3
MrOnran <100285264+MrOnran@users.noreply.github.com>
2022-12-16 23:47:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+var desc = document.getElementsByTagName("desc")
+[0];
+desc.style.fontSize = "2vw";
+desc.style.color = "#4dff88";
+document.getElementsByTagName("dslink")[0].style.fontSize = "3vw";
