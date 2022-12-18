a020c5b8142fab3d1311fc62d7d6b183a9b69355
MrOnran <100285264+MrOnran@users.noreply.github.com>
2022-12-18 13:24:25 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
-var desc = document.getElementsByTagName("desc")
-[0];
-desc.style.fontSize = "2vw";
-desc.style.color = "#4dff88";
-document.getElementsByTagName("dslink")[0].style.fontSize = "3vw";
