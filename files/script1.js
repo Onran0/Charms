97db284e5e74acb6f0c5f444d8a38ba24b9de556
MrOnran <100285264+MrOnran@users.noreply.github.com>
2022-12-18 13:24:58 +0900

    Add files via upload

---
+++ files/script1.js
@@ -0,0 +1,33 @@
+
+function enablenarrations() {
+alert("thing")
+  } 
+
+function quit() {
+  close();
+  window.location = "";
+}
+
+function discord() { 
+   window.open("https://discord.gg/85zp8xN6V9", "", "width=10000,height=10000")
+}
+
+function github() {
+   window.open("https://github.com/MrOnran/Charms", "", "width=10000,height=10000")
+}
+
+function b166() {
+   window.open("https://github.com/MrOnran/Charms/releases/tag/MC-Beta-1.6.6", "", "width=10000,height=10000")
+}
+
+function b173() {
+   window.open("https://github.com/MrOnran/Charms/releases/tag/MC-Beta-1.7.3", "", "width=10000,height=10000")
+}
+
+function b18() {
+   window.open("https://github.com/MrOnran/Charms/releases/tag/MC-Beta-1.8", "", "width=10000,height=10000")
+}
+
+function b19() {
+   window.open("https://github.com/MrOnran/Charms/releases/tag/MC-Beta-1.9", "", "width=10000,height=10000")
+}
