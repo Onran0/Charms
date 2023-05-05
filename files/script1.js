09b9ab9a3f02d89b986b223c7bf8e0fe77d43700
Onran <100285264+Onran0@users.noreply.github.com>
2023-05-05 19:04:52 +0900

    Update script1.js

--- files/script1.js
+++ files/script1.js
@@ -8,18 +8,6 @@ function quit() {
   window.location = "";
 }
 
-function discord() { 
-   window.open("https://discord.gg/85zp8xN6V9", "", "width=10000,height=10000")
-}
-
-function github() {
-   window.open("https://github.com/MrOnran/Charms", "", "width=10000,height=10000")
-}
-
-function githubdocs() {
-   window.open("https://github.com/Onran0/Charms/blob/main/README.md", "", "width=10000,height=10000")
-}
-
 function newTab(url) {
   window.open(url,'_blank').focus();
 }
