1ee9226c8eb3b9b55ef6f1463edc0a9a914bc028
Onran <100285264+Onran0@users.noreply.github.com>
2023-05-05 18:55:33 +0900

    Update script1.js

--- files/script1.js
+++ files/script1.js
@@ -16,18 +16,6 @@ function github() {
    window.open("https://github.com/MrOnran/Charms", "", "width=10000,height=10000")
 }
 
-function b166() {
-   window.open("https://github.com/MrOnran/Charms/releases/tag/MC-Beta-1.6.6", "", "width=10000,height=10000")
-}
-
-function b173() {
-   window.open("https://github.com/MrOnran/Charms/releases/tag/MC-Beta-1.7.3", "", "width=10000,height=10000")
-}
-
-function b18() {
-   window.open("https://github.com/MrOnran/Charms/releases/tag/MC-Beta-1.8", "", "width=10000,height=10000")
-}
-
-function b19() {
-   window.open("https://github.com/MrOnran/Charms/releases/tag/MC-Beta-1.9", "", "width=10000,height=10000")
+function githubdocs() {
+   window.open("https://github.com/Onran0/Charms/blob/main/README.md", "", "width=10000,height=10000")
 }
